eda42ddd7eac3cc63c67978cdd25c62bab717b00
Rajes <raorajesh7700@gmail.com>
2025-09-06 17:55:09 +0530

    added more coloumn

--- src/main/java/Uber/Uber/Entity/Review.java
+++ src/main/java/Uber/Uber/Entity/Review.java
@@ -1,11 +1,31 @@
 package Uber.Uber.Entity;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+    String reviewContent;
+
+    int rating;
+
+    @Column (nullable=false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date deletedAt;
+
+
 }
